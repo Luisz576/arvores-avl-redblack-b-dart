a04babaa62dde18f60cf2b74cfac0ccc11208627
Luisz576 <lrferlinalves@gmail.com>
2023-03-08 22:05:51 -0300

    start redblack node

--- arvores_avl_redblack_b/lib/src/redblack/node_redblack.dart
+++ arvores_avl_redblack_b/lib/src/redblack/node_redblack.dart
@@ -1,7 +1,21 @@
 import 'package:arvores_avl_redblack_b/arvores_avl_redblack_b.dart';
 
 class NodeRedBlack<V> extends Node<V>{
-  NodeAVL? left, right;
+  bool isRed = true;
+  NodeRedBlack<V>? parent, _left, _right;
+
+  NodeRedBlack<V>? get left => _left;
+  NodeRedBlack<V>? get right => _right;
+
+  set left(NodeRedBlack<V>? node){
+    _left = node;
+    _left?.parent = this;
+  }
+
+  set right(NodeRedBlack<V>? node){
+    _right = node;
+    _right?.parent = this;
+  }
 
   NodeRedBlack(super.id, super.value);
 }
